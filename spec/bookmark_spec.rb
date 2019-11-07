bf04c0d780090bc90743d7b6f7e036f524e5b116
Donald Newholm <newholm1@hotmail.com>
2019-11-07 16:59:04 +0000

    Step 11 complete except for refactoring to helper method

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,22 +7,30 @@ describe '.all' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
     # Add the test data
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers")
+    Bookmark.create(url: "http://destroyallsoftware.com", title: "DESTROY!!!!")
+    Bookmark.create(url: "http://www.google.com", title: "Google")
 
     bookmarks = Bookmark.all
 
-    expect(bookmarks).to include('http://www.makersacademy.com')
-    expect(bookmarks).to include('http://www.destroyallsoftware.com')
-    expect(bookmarks).to include('http://www.google.com')
+    expect(bookmarks.length).to eq 3
+    expect(bookmarks.first).to be_a Bookmark
+    expect(bookmarks.first.id).to eq bookmark.id
+    expect(bookmarks.first.title).to eq 'Makers'
+    expect(bookmarks.first.url).to eq "http://www.makersacademy.com"
   end
 end
 
 describe '.create' do
   it 'creates a new bookmark' do
-    Bookmark.create(url: 'http://www.testbookmark.com')
-
-    expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'test title')
+    persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+    expect(bookmark).to be_a Bookmark
+    expect(bookmark.id).to eq persisted_data.first['id']
+    expect(bookmark.url).to eq 'http://www.testbookmark.com'
+    expect(bookmark.title).to eq 'test title'
   end
 end
